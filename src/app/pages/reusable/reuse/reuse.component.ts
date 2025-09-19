c3e05546f2dc84b871c0577f533e457f02d47825
Himanshu Jindal <Himanshu.Jindal@intimetec.com>
2025-09-19 09:49:24 +0530

    reusable component added

---
+++ src/app/pages/reusable/reuse/reuse.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-reuse',
+  imports: [RouterLink],
+  templateUrl: './reuse.component.html',
+  styleUrl: './reuse.component.css'
+})
+export class ReuseComponent {
+
+
+}
